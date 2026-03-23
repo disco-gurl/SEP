1c84fd888456859e12b7008ec02480240fd36dcd
Dianqing <zengdq21@gmail.com>
2026-03-23 15:50:39 +0000

    Some getters and setters

--- src/main/java/User/Student.java
+++ src/main/java/User/Student.java
@@ -1,11 +1,35 @@
 package User;
 
-public class Student extends User {
-    StudentPreferences studentPreferences;
-    public String name;
-    public int phoneNumber;
+import Booking.Booking;
+import java.util.ArrayList;
+import java.util.Collection;
 
-    public Student(String email, String password) {
+public class Student extends User {
+    private StudentPreferences studentPreferences;
+    private String name;
+    private int phoneNumber;
+    private Collection<Booking> bookings;
+
+    public Student(String email, String password, String name, int phonenumber) {
         super(email, password);
+        this.name = name;
+        this.phoneNumber = phonenumber;
+        this.bookings = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void addBooking(Booking booking) {
+        bookings.add(booking);
+    }
+
+    public Collection<Booking> getBookings() {
+        return bookings;
     }
 }
