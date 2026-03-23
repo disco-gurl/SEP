fb305d2333b02ee6340b9f25d53e7a15748fc4e2
Dianqing <zengdq21@gmail.com>
2026-03-23 14:14:16 +0000

    Made some changes in User, need the relevant methods please dont delete.

--- src/main/java/User/User.java
+++ src/main/java/User/User.java
@@ -1,9 +1,37 @@
 package User;
 
 public abstract class User {
-    public String email;
-    public String password;
+    private String email;
+    private String password;
 
-    public User(String email, String password) {
+    protected User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Checks whether the password matches
+     *
+     * @param password
+     * @return 
+     */
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
     }
 }
