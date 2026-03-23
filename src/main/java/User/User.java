1edc554cfe4b876471801ddaab222e585a08b59b
Dianqing <zengdq21@gmail.com>
2026-03-23 12:07:32 +0000

    Added relevant getter and setters in User

--- src/main/java/User/User.java
+++ src/main/java/User/User.java
@@ -1,6 +1,44 @@
 package User;
 
-abstract class User {
+public abstract class User {
     private String email;
     private String password;
+
+    /**
+     * Constructor with the email and password given.
+     *
+     * @param email
+     * @param password
+     */
+    protected User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Checks the provided password matches the user's.
+     *
+     * @param password
+     * @return matches?
+     */
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
 }
