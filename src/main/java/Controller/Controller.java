e317d2148e44660b5dbe073bc78431747c06fc16
Dianqing <zengdq21@gmail.com>
2026-03-23 12:39:52 +0000

    Added controller methods with getters/setters

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,11 +1,61 @@
 package Controller;
 
-abstract class Controller {
+import User.User;
+import User.AdminStaff;
+import User.Student;
+import User.EntertainmentProvider;
+import View.View;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public abstract class Controller {
     private User currentUser;
     private View view;
 
-    protected Controller(View view){
+    /**
+     * The constructor of a controller with the given view.
+     *
+     * @param view
+     */
+    protected Controller(View view) {
         this.currentUser = null;
         this.view = view;
     }
+
+    protected View getView() {
+        return view;
+    }
+
+    protected User getCurrentUser() {
+        return currentUser;
+    }
+
+    protected void setCurrentUser(User user) {
+        this.currentUser = user;
+    }
+
+    /**
+     * Methods from class diagram.
+     *
+     * @return
+     */
+    protected boolean checkCurrentUserIsGuest() {
+        return currentUser == null;
+    }
+
+    private boolean checkCurrentUserIsAdmin() {
+        return currentUser instanceof AdminStaff;
+    }
+
+    private boolean checkCurrentUserIsStudent() {
+        return currentUser instanceof Student;
+    }
+
+    private boolean checkCurrentUserIsEntertainmentProvider() {
+        return currentUser instanceof EntertainmentProvider;
+    }
+
+
 }
